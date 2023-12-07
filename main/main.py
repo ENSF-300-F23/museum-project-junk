7ab197a6da4ca765410e427accbcd4959ecb3f89
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2023-12-07 10:29:19 -0700

    Added log in menu and validations

--- main/main.py
+++ main/main.py
@@ -1,18 +1,50 @@
 import mysql.connector
 from mysql.connector import errorcode
 
-try:
-    cnx = mysql.connector.connect(user='museum_admin', password='password',
-                                 host='127.0.0.1',
-                                 database='MUSEUM')
 
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
-    else:
-        print(err)
+if __name__ == "__main__":
 
-else:
-    cnx.close()
+    # Login menu
+    print("Welcome to the Museum Database:")
+    print("In order to proceed please select your role from the list below:")
+    print("1 - DB Admin")
+    print("2 - Employee")
+    print("3 - Browse as guest")
+
+    # User's choice
+    selection = input("\nPlease type 1, 2, or 3 to select your role: ")
+
+    while selection not in ["1", "2", "3"]:
+        selection = input("Please type 1, 2, or 3 to select your role: ")
+
+    
+    # Connect with database
+    attempts = 5
+    while attempts:
+        try:
+            # Get login details
+            if selection in ["1", "2"]:
+                username = input("Username: ")
+                passcode = input("Password: ")
+            else:
+                username = 'guest'
+                passcode = None
+
+            cnx = mysql.connector.connect(user=username, password=passcode,
+                                        host='127.0.0.1',
+                                        database='MUSEUM')
+
+        except mysql.connector.Error as err:
+            attempts -= 1
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            else:
+                print(err)
+            print('')
+
+        else:
+            cnx.close()
+
+    # If number of attempts are exceeded
+    print("Number of attempts exceeded")
+    print("\nProgram terminated")
