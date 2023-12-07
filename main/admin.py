1851df81a552f47713836ffc4ad7466e99fdad08
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2023-12-07 14:56:47 -0700

    Updated the admin interface to enable sql entry display

--- main/admin.py
+++ main/admin.py
@@ -1,2 +1,26 @@
 def execute_admin_choice(choice, conn):
-    pass
+    # Get a cursor
+    cursor = conn.cursor()
+        
+    # Execute a query
+    cursor.execute("USE MUSEUM")
+
+    if choice == "1":
+        print("")
+
+        while True:
+            sql_command = input("Enter SQL command ('q' to quit): ")
+            if sql_command.lower() == 'q':
+                break
+
+            cursor.execute(sql_command)
+            if cursor.description is not None:
+                rows = cursor.fetchall()
+                for row in rows:
+                    print(row)  # Display fetched rows for SELECT queries
+
+            conn.commit()  # Commit changes
+
+    
+    elif choice == "2":
+        pass
